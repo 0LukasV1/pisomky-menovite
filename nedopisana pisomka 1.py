837d04ab51728ee62ec9d9c07094c6d984a6f6d9
0LukasV1 <113472942+0LukasV1@users.noreply.github.com>
2022-12-14 09:24:19 +0100

    Add files via upload

---
+++ nedopisana pisomka 1.py
@@ -0,0 +1,18 @@
+# input ak velke pismebko posun ho o 4 miesta
+a = 95
+def pismenko(text):
+    for i in range(0, len(text)):
+        x = ord(text[i])
+        if a >= x:
+            print(i,end=", ")
+            print(+4,end=", ")
+            print(text[i],end=", ")
+            print()
+        else:
+            print(i,end=", ")
+            print(x,end=", ")
+            print(text[i],end=", ")
+            print()
+
+
+pismenko(input('Zadaj vetu: ' ))
